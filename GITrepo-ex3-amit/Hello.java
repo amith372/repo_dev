3b84574c1581f69488f311ea20fc2d7e12f72be4
amith372 <amith9435@gmail.com>
2026-04-20 10:32:31 +0300

    moved hello.java

--- GITrepo-ex3-amit/Hello.java
+++ GITrepo-ex3-amit/Hello.java
@@ -1,7 +1,7 @@
-public class amit{
+public class Hello{
 	public static void main(String[] args) {
 		for (int i=1;i<=10;i++){
-			System.out.println("amit");
+			System.out.println("hello World...update"+i);
 		}
 	}
 }
